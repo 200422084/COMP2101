2571eb10aae2add162e68361746ba624a10ab0b6
200422084 <rjohny387@gmail.com>
2020-05-29 13:08:09 -0700

    ugly script

---
+++ bash/helloworldugly.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#My Third Script- Helloworldugly.sh
+#This Script displayes the string "Hello World"
+#This is a silly way of creating an output text by starting with something else and stream editing it in a pipeline
+echo -n "helb wold"
+sed -e "s/b/o/g" -e "/s/l/ll" -e "/s/ol/orl/"
+tr "h" "H" | tr "w" "W" | awk '{print $1 "\x20" $2 "\41"}'
+bc <<< "(($$ * 4 - 24)/2 + 12)/2" | sed 's/^/I am process #/'
